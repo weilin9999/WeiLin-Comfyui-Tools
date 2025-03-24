f14dfa5c7cc1e9555312003f6371d5ae198ae83a
WeiLin <335663085@qq.com>
2025-03-24 18:43:56 +0800

    增量更新v0.0.22
    增量更新v0.0.22

--- app/server/translate/api_translate.py
+++ app/server/translate/api_translate.py
@@ -11,36 +11,52 @@ def translateText(text,service,source,target):
 
 
 translatePackages =  {
-    "translators": "translators"
+    "translators": {"package": "translators", "version": ">=5.9.9"}
 }
 
 def checkHasInstallTranslateApi():
     states = []
     for package_name in translatePackages:
-        package = translatePackages[package_name]
+        package_info = translatePackages[package_name]
         item = {
             'name': package_name,
-            'package': package,
+            'package': package_info['package'],
             'state': False
         }
-        if is_installed("translators"):
-            item['state'] = True
+        if is_installed(package_info['package']):
+            import pkg_resources
+            try:
+                installed_version = pkg_resources.get_distribution(package_info['package']).version
+                if pkg_resources.parse_version(installed_version) >= pkg_resources.parse_version("5.9.9"):
+                    item['state'] = True
+            except Exception as e:
+                print(f"Error checking {package_name} version: {e}")
+                item['state'] = False
 
         states.append(item)
 
     return states
 
-
 def installTranslateApi():
     try:
-        __import__("translators")
+        import pkg_resources
+        installed_version = pkg_resources.get_distribution("translators").version
+        if pkg_resources.parse_version(installed_version) < pkg_resources.parse_version("5.9.9"):
+            print("translators版本过低，正在升级...")
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'translators>=5.9.9'])
     except ImportError:
         print("translators包未安装，正在安装...")
-        print("translators package is not install，is install...")
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'translators'])
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'translators>=5.9.9'])
     print("WeiLin-Comfyui-Tools ==> translators 已安装成功。")
 
 def applyTranslateApi():
     if is_installed("translators"):
-        return True
+        import pkg_resources
+        try:
+            installed_version = pkg_resources.get_distribution("translators").version
+            if pkg_resources.parse_version(installed_version) >= pkg_resources.parse_version("5.9.9"):
+                return True
+        except Exception as e:
+            print(f"当前translators库版本过低: {e}")
+            print(f"Error checking translators version: {e}")
     return False
